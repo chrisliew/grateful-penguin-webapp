40ac09ed824473742aec5a6799f3b654ded1da3b
chrisliew <chriswvliew@gmail.com>
2019-07-19 10:11:46 +0700

    added gitignore

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hi: "there!" });
+  res.send({ hi: "there!" nodemo});
 });
 
+
+const PORT = process.env.PORT || 5000;
 app.listen(5000);
